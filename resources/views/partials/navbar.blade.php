e1d20ab4068c0b67fe0840f0bf3a62bddabfb08c
Eelyneee <79684593+Eelyneee@users.noreply.github.com>
2022-04-12 10:44:24 +0800

    Update navbar.blade.php
    remove about and contact

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -11,18 +11,9 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
                 @can('patient-access')
                     <li class="nav-item">
                         <a class="nav-link" href="{{route('patient.home')}}">Appointments</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact Us</a>
                     </li>
                 @endauth
                 @can('admin-access')
