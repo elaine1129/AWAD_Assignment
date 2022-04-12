47082c9ee592ee8e9e50e973f97e95e0cc4641c5
kai <kaienyeo225@gmail.com>
2022-04-12 11:35:00 +0800

    Modify reset password view

--- resources/views/common/edit-password.blade.php
+++ resources/views/common/edit-password.blade.php
@@ -4,40 +4,37 @@
 @section('body')
     @include('partials.errors')
     @include('partials.success')
-    <form action="{{route('common.edit-password')}}" method="POST">
-        @csrf
-        @method('PUT')
-        <div class="label">
-            <label for="email"> Email: </label>
-        </div>
-        <div class="input">
-            <input disabled type="email" placeholder="Enter your email" name="email" required value="{{$email}}" >
-        </div>
+    <div class="container row">
+        <h1 class="col-12">Reset Password</h1>
+        <form action="{{route('common.edit-password')}}" method="POST" class="col-md-8 mx-auto">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="email"> Email: </label>
+                <input class="form-control" disabled type="email" placeholder="Enter your email" name="email" required value="{{$email}}" >
+            </div>
 
-        <div class="label">
-            <label for="password">Old Password*:</label>
-        </div>
-        <div class="input">
-            <input type="password" placeholder="Minimum 6 characters" name="old_password">
-        </div>
+            <div class="form-group">
+                <label for="password">Old Password*:</label>
+                <input class="form-control" type="password" placeholder="Minimum 6 characters" name="old_password">
+            </div>
 
-        <div class="label">
-            <label for="password">New Password*:</label>
-        </div>
-        <div class="input">
-            <input type="password" placeholder="Minimum 6 characters" name="password">
-        </div>
+            <div class="form-group">
+                <label for="password">New Password*:</label>
+                <input class="form-control" type="password" placeholder="Minimum 6 characters" name="password">
+            </div>
 
-        <div class="label">
-            <label for="password">Password confirmation*:</label>
-        </div>
-        <div class="input">
-            <input type="password" placeholder="Minimum 6 characters" name="password_confirmation">
-        </div>
+            <div class="form-group">
+                <label for="password">Password confirmation*:</label>
+                <input class="form-control" type="password" placeholder="Minimum 6 characters" name="password_confirmation">
+            </div>
 
-        <button type="submit">Edit</button>
-        <button type="button" onclick="history.back()">Back</button>
-    </form>
+            <div class="mt-2">
+                <button class="btn btn-primary" type="submit">Edit</button>
+                <button class="btn" type="button" onclick="history.back()">Back</button>
+            </div>
+        </form>
+    </div>
 @endsection
 @section('script')
     <script>
